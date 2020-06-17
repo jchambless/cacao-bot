47368adacc9c2c78308062d1ac31d3e7351cc8e7
James Chambless <j.chambless@gmail.com>
2020-06-16 20:56:00 -0400

    Added a command list on help command

--- commands/help.go
+++ commands/help.go
@@ -9,5 +9,18 @@ import (
 func HelpCommand(ctx framework.Context) {
 	log.Println("Help command was called.")
 
-	ctx.Reply(ctx.Conf.BotName + " running v " + ctx.Conf.Version)
+	var message string
+
+	// Todo: Build the string output before sending a reply
+	message = ctx.Conf.BotName + " running v " + ctx.Conf.Version + "\n"
+	message += "**Command List**\n"
+
+	cmds := ctx.CmdHandler.GetCmds()
+	for key, element := range cmds {
+		if len(key) > 1 {
+			message += "- " + key + ": " + element.GetHelp() + "\n"
+		}
+	}
+
+	ctx.Reply(message)
 }
